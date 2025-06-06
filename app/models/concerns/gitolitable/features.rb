84f5de0ea0109484e1a12c28583a0a7282757be0
Tomas <tomdem@protonmail.com>
2025-06-06 12:29:42 +0000

    fixed nil error

--- app/models/concerns/gitolitable/features.rb
+++ app/models/concerns/gitolitable/features.rb
@@ -15,39 +15,39 @@ module Gitolitable
     end
 
     def git_default_branch
-      extra[:default_branch]
+      safe_extra[:default_branch]
     end
 
     def gitolite_hook_key
-      extra[:key]
+      safe_extra[:key]
     end
 
     def git_daemon_enabled?
-      extra[:git_daemon]
+      safe_extra[:git_daemon]
     end
 
     def git_annex_enabled?
-      extra[:git_annex]
+      safe_extra[:git_annex]
     end
 
     def git_notification_enabled?
-      extra[:git_notify]
+      safe_extra[:git_notify]
     end
 
     def git_ssh_enabled?
-      extra[:git_ssh]
+      safe_extra[:git_ssh]
     end
 
     def git_go_enabled?
-      extra[:git_go]
+      safe_extra[:git_go]
     end
 
     def https_access_enabled?
-      extra[:git_https]
+      safe_extra[:git_https]
     end
 
     def http_access_enabled?
-      extra[:git_http]
+      safe_extra[:git_http]
     end
 
     def smart_http_enabled?
@@ -63,7 +63,7 @@ module Gitolitable
     end
 
     def protected_branches_enabled?
-      extra[:protected_branch]
+      safe_extra[:protected_branch]
     end
 
     def public_project?
@@ -71,11 +71,17 @@ module Gitolitable
     end
 
     def public_repo?
-      extra[:public_repo]
+      safe_extra[:public_repo]
     end
 
     def urls_order
-      extra[:urls_order]
+      safe_extra[:urls_order]
+    end
+
+    private
+
+    def safe_extra
+      extra || {}
     end
   end
 end
